268bd93c8cb1ba073f201293160960099b3fe99d
ANGELO ISRAEL CONTERON SANTILLAN <angelo.conteron@epn.edu.ec>
2025-11-10 17:49:45 -0500

    feat(router): rutas conectadas y estilos base

--- Proyecto/festimap-react/src/main.jsx
+++ Proyecto/festimap-react/src/main.jsx
@@ -1,14 +1,35 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Landing from './pages/Landing.jsx'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Intereses from './pages/Intereses.jsx'
+import Region from './pages/Region.jsx'
+import Tema from './pages/Tema.jsx'
+import DetalleEvento from './pages/DetalleEvento.jsx'
+import PlanViaje from './pages/PlanViaje.jsx'
+import Cuenta from './pages/Cuenta.jsx'
+
+import './styles/reset.css'
+import './styles/tokens.css'
+import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/intereses" element={<Intereses />} />
+        <Route path="/region" element={<Region />} />
+        <Route path="/tema" element={<Tema />} />
+        <Route path="/evento/:id" element={<DetalleEvento />} />
+        <Route path="/plan" element={<PlanViaje />} />
+        <Route path="/cuenta" element={<Cuenta />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 )
